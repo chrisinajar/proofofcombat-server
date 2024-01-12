9f11d90bc71732f1056c9939c59c23ff2071a986
Chris Vickery <chris.vickery@dutchie.com>
2024-01-12 13:55:15 -0800

    implement generic stats modifier

--- calculations/modifiers/generic-stats-modifier.ts
+++ calculations/modifiers/generic-stats-modifier.ts
@@ -1,18 +1,39 @@
 import { Modifier, ModifierOptions } from "./modifier";
 
-export type GenericStatsModifierOptions = {};
+type StatsEntry = {
+  [x in string]: number;
+};
+
+export type GenericStatsModifierOptions = {
+  multiplier?: StatsEntry;
+  bonus?: StatsEntry;
+  extraBonus?: StatsEntry;
+};
 
 export class GenericStatsModifier extends Modifier<GenericStatsModifierOptions> {
+  options: GenericStatsModifierOptions;
+
   constructor(options: ModifierOptions<GenericStatsModifierOptions>) {
     super(options);
+
+    this.options = options.options;
   }
   getBonus(prop: string): number | void {
+    if (this.options.bonus && this.options.bonus[prop]) {
+      return this.options.bonus[prop];
+    }
     return;
   }
   getMultiplier(prop: string): number | void {
+    if (this.options.multiplier && this.options.multiplier[prop]) {
+      return this.options.multiplier[prop];
+    }
     return;
   }
   getExtraBonus(prop: string): number | void {
+    if (this.options.extraBonus && this.options.extraBonus[prop]) {
+      return this.options.extraBonus[prop];
+    }
     return;
   }
 }
