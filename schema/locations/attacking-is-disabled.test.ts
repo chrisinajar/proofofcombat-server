b104c9aa7104cb0f0d34fa5949477129cf0a1818
Chris Vickery <chris.vickery@dutchie.com>
2024-02-28 00:38:15 -0800

    .... who cant spell

--- schema/locations/attacking-is-disabled.test.ts
+++ schema/locations/attacking-is-disabled.test.ts
@@ -1,7 +1,7 @@
-import { attackingIsDisabled } from "./resolvers";
+import { attackingIsDisabled } from "./ resolvers";
 
 describe("attacking being disabled", () => {
-  it("didn't get let enabled by accident while i was testing stuff", () => {
+  it("didn't get left enabled by accident while i was testing stuff", () => {
     expect(attackingIsDisabled).toBe(true);
   });
 });
